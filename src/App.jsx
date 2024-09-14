1e199c3ce1da79b59b3fdad234b3fc786f5162af
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-09-14 16:52:17 +0700

    component & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+
+const Button = props => {
+	// Destructuring
+	const { children = '...', variant = 'bg-black' } = props;
+	return (
+		<button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+			{children}
+		</button>
+	);
+};
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+		<div className="flex justify-center bg-blue-600 min-h-screen items-center text-white">
+			<div className="flex gap-x-4">
+				<Button variant="bg-red-700">Login</Button>
+				<Button variant="bg-slate-700">Logout</Button>
+				<Button>Register</Button>
 			</div>
-			<h1>M. Zaky Pria Maulana</h1>
-			<div className="card">
-				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+		</div>
 	);
 }
 
