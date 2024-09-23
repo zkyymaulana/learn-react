29c03ebdde7f63e6434852e190eea1eec9361f72
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-09-24 06:32:57 +0700

    rendering lists

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,39 @@
 import CardProduct from '../components/Fragments/CardProduct';
 
+const products = [
+	{
+		id: 1,
+		name: 'Sepatu Baru',
+		price: 'Rp. 1.000.000',
+		image: '/images/shoes-1.jpg',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nostrum qui earum, itaque perspiciatis dignissimos eum fugiat totam asperiores laborum animi?`,
+	},
+	{
+		id: 2,
+		name: 'Sepatu Olahraga Pro',
+		price: 'Rp. 1.200.000',
+		image: '/images/shoes-1.jpg',
+		description: `Sepatu ini dirancang khusus untuk aktivitas olahraga yang intens dengan kenyamanan optimal.`,
+	},
+	{
+		id: 3,
+		name: 'Sepatu Kulit Premium',
+		price: 'Rp. 950.000',
+		image: '/images/shoes-1.jpg',
+		description: `Sepatu kulit asli berkualitas tinggi yang cocok untuk acara formal dan kasual.`,
+	},
+];
+
 const ProductsPage = () => {
 	return (
 		<div className="flex justify-center py-5">
-			<CardProduct>
-				<CardProduct.Header image="/images/shoes-1.jpg" />
-				<CardProduct.Body title="Sepatu Baru">ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt soluta vel consequatur dicta inventore? Qui autem sit aperiam ex vero!</CardProduct.Body>
-				<CardProduct.Footer price="Rp. 1.000.000" />
-			</CardProduct>
-			<CardProduct>
-				<CardProduct.Header image="/images/shoes-1.jpg" />
-				<CardProduct.Body title="Sepatu Baru">ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt soluta vel consequatur dicta inventore? Qui autem sit aperiam ex vero!</CardProduct.Body>
-				<CardProduct.Footer price="Rp. 1.000.000" />
-			</CardProduct>
+			{products.map(product => (
+				<CardProduct>
+					<CardProduct.Header image={product.image} />
+					<CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+					<CardProduct.Footer price={product.price} />
+				</CardProduct>
+			))}
 		</div>
 	);
 };
