68aa930f80ac893bf8fb9c92a6077135359498f5
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-20 10:40:15 +0700

    post api

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -3,16 +3,25 @@ import Button from '../components/Elements/Button/Index';
 import CardProduct from '../components/Fragments/CardProduct';
 import Counter from '../components/Fragments/Counter';
 import { getProducts } from '../services/product.service';
-
-const email = localStorage.getItem('email');
+import { getUsername } from '../services/auth.service';
 
 const ProductsPage = () => {
 	const [cart, setCart] = useState([]);
 	const [totalPrice, setTotalPrice] = useState(0);
 	const [products, setProducts] = useState([]);
+	const [username, setUsername] = useState('');
 
 	useEffect(() => {
 		setCart(JSON.parse(localStorage.getItem('cart')) || []);
+	}, []);
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			setUsername(getUsername(token));
+		} else {
+			window.location.href = '/login';
+		}
 	}, []);
 
 	useEffect(() => {
@@ -33,7 +42,7 @@ const ProductsPage = () => {
 	}, [cart, products]);
 
 	const handleLogout = () => {
-		localStorage.removeItem('email');
+		localStorage.removeItem('token');
 		localStorage.removeItem('password');
 		window.location.href = '/login';
 	};
@@ -69,7 +78,7 @@ const ProductsPage = () => {
 	return (
 		<Fragment>
 			<div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-				{email}
+				{username}
 				<Button classname="ml-5 bg-black" onClick={handleLogout}>
 					Logout
 				</Button>
