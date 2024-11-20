863da04ffa347605891ba0c4bc889bb062b4fc95
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-20 11:51:07 +0700

    dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import RegisterPage from './Pages/Register.jsx';
 import ErrorPage from './Pages/404.jsx';
 import ProductsPage from './Pages/products';
 import ProfilePage from './Pages/profile';
+import DetailProductPage from './Pages/detailProduct';
 
 const router = createBrowserRouter([
 	{
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
 	{
 		path: '/profile',
 		element: <ProfilePage />,
+	},
+	{
+		path: '/product/:id',
+		element: <DetailProductPage />,
 	},
 ]);
 createRoot(document.getElementById('root')).render(
