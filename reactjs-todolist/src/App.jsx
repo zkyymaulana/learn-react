f8fc6f568d4a6a62df4dbda85973449cbc85391e
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-22 17:34:15 +0700

    reinit project to-do-list using react and tailwindcss

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,11 +1,8 @@
-import './App.css';
-import Form from './components/Form';
-
 function App() {
 	return (
-		<>
-			<Form />
-		</>
+		<div>
+			<h1 className="text-4xl text-red-400 font-bold">Hello World!</h1>
+		</div>
 	);
 }
 
