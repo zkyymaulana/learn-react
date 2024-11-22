4e4b624bbe1c6bec5656123f5ca0dcedd9ae1925
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-22 10:56:37 +0700

    add component form

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-	return <div>App</div>;
+	return (
+		<>
+			<Form />
+		</>
+	);
 }
 
 export default App;
---
+++ reactjs-todolist/src/components/Form.jsx
@@ -0,0 +1,44 @@
+function Form() {
+	return (
+		<div className="wrapper">
+			<ul>
+				<li>
+					<div class="left">
+						<button>✅</button>
+					</div>
+					<div className="center">bbb</div>
+					<div className="right">
+						<span>
+							<button>👆</button>
+						</span>
+						<span>
+							<button>👇</button>
+						</span>
+						<span>
+							<button>🗑️</button>
+						</span>
+					</div>
+				</li>
+				<li>
+					<div class="left">
+						<button>◻️</button>
+					</div>
+					<div className="center">aaa</div>
+					<div className="right">
+						<span>
+							<button>👆</button>
+						</span>
+						<span>
+							<button>👇</button>
+						</span>
+						<span>
+							<button>🗑️</button>
+						</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Form;
