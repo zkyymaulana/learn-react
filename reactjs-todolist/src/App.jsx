d9e60d456aee4acdd40f5d30447897710fc2b2c3
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-23 20:12:03 +0700

    feat: todolist | add create data

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,17 +1,35 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
 	const newTask = useRef('');
+	const [tasks, setTasks] = useState([]);
+
+	function setId() {
+		const jumlahTask = tasks.length;
+		return jumlahTask + 1;
+	}
+
 	function addTask(event) {
 		event.preventDefault();
-		console.log(`saya diklik isi ${newTask.current.value}`);
+		if (newTask.current.value === '') {
+			alert('Please enter what you are going to do');
+			return false;
+		}
+		const data = {
+			id: setId(),
+			task: newTask.current.value,
+			isCompleted: false,
+		};
+		newTask.current.value = '';
+		setTasks([...tasks, data]);
 	}
+
 	return (
 		<div>
 			<Form addTask={addTask} newTask={newTask} />
-			<ToDoList />
+			<ToDoList tasks={tasks} />
 		</div>
 	);
 }
