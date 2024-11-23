30df982fd5173c7d55a8d4039de8533dbc13ad24
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-23 17:46:02 +0700

    feat: todolist | add process of receiving input

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,10 +1,16 @@
+import { useRef } from 'react';
 import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+	const newTask = useRef('');
+	function addTask(event) {
+		event.preventDefault();
+		console.log(`saya diklik isi ${newTask.current.value}`);
+	}
 	return (
 		<div>
-			<Form />
+			<Form addTask={addTask} newTask={newTask} />
 			<ToDoList />
 		</div>
 	);
--- reactjs-todolist/src/components/Form.jsx
+++ reactjs-todolist/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 
-function Form() {
+function Form(props) {
 	return (
 		<div className="flex items-center justify-center p-6 mt-10">
 			<div className="w-full max-w-lg bg-white p-6 rounded-lg shadow-md">
@@ -9,10 +9,12 @@ function Form() {
 					<img src={logo} alt="Logo" className="mb-4 w-14" />
 					<h1 className="text-4xl font-semibold text-blue-950 mb-6">MyTask</h1>
 				</div>
-				<input type="text" placeholder="Add Your Task" className="w-full p-2 mb-4 border-b-2 border-gray-300 focus:outline-none text-gray-700 placeholder-gray-400" />
-				<button type="submit" className="w-full py-2 bg-blue-900 text-white rounded-lg hover:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-blue-300">
-					Add Task
-				</button>
+				<form action="">
+					<input type="text" ref={props.newTask} placeholder="Add Your Task" className="w-full p-2 mb-4 border-b-2 border-gray-300 focus:outline-none text-gray-700 placeholder-gray-400" />
+					<button type="submit" className="w-full py-2 bg-blue-900 text-white rounded-lg hover:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-blue-300" onClick={props.addTask}>
+						Add Task
+					</button>
+				</form>
 			</div>
 		</div>
 	);
