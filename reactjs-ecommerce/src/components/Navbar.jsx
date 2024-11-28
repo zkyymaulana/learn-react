294cb64bbcf86c4b508becb5bf80ddbbb1e1bcd3
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-11-28 10:26:17 +0700

    feat: ecommerce | responsive navbar

---
+++ reactjs-ecommerce/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Logo from '../assets/logo.png';
+import { IoMdSearch } from 'react-icons/io';
+import { FaCartShopping } from 'react-icons/fa6';
+
+function Navbar() {
+	return (
+		<div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+			{/* upper Navbar */}
+			<div className="bg-primary/40 py-2">
+				<div className="container flex justify-between items-center">
+					<div>
+						<a href="#" className="font-bold text-xl sm:text-2xl flex gap-2 items-center">
+							<img src={Logo} alt="Logo" className="w-8 uppercase" />
+							Kymol'Store
+						</a>
+					</div>
+					{/* search bar */}
+					<div className="flex justify-between items-center gap-4">
+						<div className="relative group hidden sm:block">
+							<input
+								type="text"
+								placeholder="search"
+								className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-4 py-1 focus:outline-none focus:border-1 focus:border-primary"
+							/>
+							<IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+						</div>
+						{/* order button */}
+						<button onClick={() => alert('Ordering not available yet')} className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group">
+							<span className="group-hover:block hidden transition-all duration-200">Order</span>
+							<FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer"></FaCartShopping>
+						</button>
+					</div>
+				</div>
+			</div>
+			{/* lower Navbar */}
+			<div></div>
+		</div>
+	);
+}
+
+export default Navbar;
--- reactjs-ecommerce/tailwind.config.js
+++ reactjs-ecommerce/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#77EC6A',
+				secondary: '#2BC71A',
+			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: '1rem',
+					sm: '3rem',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
